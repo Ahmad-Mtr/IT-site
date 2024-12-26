6f167b414c24f5a95e09b05967c01e1bf6f083ab
AhmadMtr <70106976+Ahmad-Mtr@users.noreply.github.com>
2024-12-26 15:09:16 +0000

    update: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel';
-
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://positivustheme.vercel.app",
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel(),
 });
